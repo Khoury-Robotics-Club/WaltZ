d9c907ba28102e4b57c68ba157c9ed467b2009f8
ashwinrb7799@gmail.com <ashwinrb7799@gmail.com>
2024-05-20 12:56:32 -0400

    Simulation improved

--- Code/simulation/simulation.py
+++ Code/simulation/simulation.py
@@ -8,6 +8,13 @@ planeId = p.loadURDF("plane.urdf")
 startPos = [0,0,10]
 startOrientation = p.getQuaternionFromEuler([0,0,0])
 boxId = p.loadURDF("./waltz.urdf.xml", startPos, startOrientation)
+
+joint_name = "revoluteRightHip"  # Replace with the actual joint name
+joint_index = p.getJointInfo(boxId, p.JOINT_REVOLUTE)[0]  # Get the joint index
+
+# Set joint motor control (e.g., velocity control with zero target velocity for joint friction)
+p.setJointMotorControl2(boxId, joint_index, controlMode=p.VELOCITY_CONTROL, targetVelocity=10, force=0)
+
 for i in range (10000):
     p.stepSimulation()
     time.sleep(1./240.)
